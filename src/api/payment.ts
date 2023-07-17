48956b44c1b19f1c31f45d170f74ffdcaad65956
Jiwon <87058411+raipen@users.noreply.github.com>
2023-07-17 15:50:57 +0900

    Feat: menu R, payment CR api (#1)
    * feat:create seed file and update models
    
    * feat: update menu testing
    
    * feat: create payment test
    
    * feat: implement create(POST) Payment API
    
    * feat: implement get all payment api

--- src/api/payment.ts
+++ src/api/payment.ts
@@ -1,6 +1,90 @@
 import { FastifyInstance,FastifyPluginAsync, FastifyRequest,FastifyReply } from "fastify";
+import { Menu } from '@prisma/client';
 
 const api: FastifyPluginAsync =  async (server: FastifyInstance) => {
+    server.post<{
+        Body: {
+            storeId: number;
+            menus: Array<{id:number,count:number}>;
+        },
+        Reply: {
+            menus: Array<Object>;
+        }
+    }>('/', async (request, reply) => {
+        const storeId = request.body.storeId;
+        const menus: {id:number,count:number}[] = request.body.menus;
+        const menusFromDatabase: Menu[] = await server.prisma.menu.findMany({
+            where: {
+                id: {
+                    in: menus.map((menu) => menu.id)
+                }
+            }
+        });
+        if(menus.filter((menu) => menusFromDatabase.find((menuFromDatabase) => menuFromDatabase.id === menu.id) === undefined).length > 0) {
+            return reply
+                .code(404)
+                .send();
+        }
+
+        const realMenus: {menuId:number, count:number, price:number}[] = menus.map((menu) => {
+            return {
+                menuId: menu.id,
+                count: menu.count,
+                price: Number(menusFromDatabase.find((menuFromDatabase) => menuFromDatabase.id === menu.id)?.price)
+            }
+        });
+        const totalPrice = realMenus.reduce((acc, cur) => acc + cur.price * cur.count, 0);
+
+        const payment = await server.prisma.payment.create({
+            data: {
+                storeId: storeId,
+                paymentMethod: "credit",
+                paymentStatus: "paid",
+                totalPrice: totalPrice,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                orderitems: {
+                    create: realMenus
+                }
+            },
+            include: {
+                orderitems: true
+            }
+        });
+        reply.code(200).send(payment);
+    });
+
+    server.get<{
+        Params: {
+            storeId: string;
+        },
+        Reply: {
+            menus: Array<Object>;
+        }
+    }>('/:storeId', async (request, reply) => {
+        const storeId = Number(request.params.storeId);
+        if(await server.prisma.store.findFirst({
+            where: {
+                id: storeId
+            }
+        }) === null) {
+            return reply
+                .code(404)
+                .send();
+        }
+
+        const payments = await server.prisma.payment.findMany({
+            where: {
+                storeId: storeId
+            },
+            include: {
+                orderitems: true
+            }
+        });
+        reply
+            .code(200)
+            .send(payments);
+    });
 }
 
 export default api;
--- test/menuService.test.ts
+++ test/menuService.test.ts
@@ -1,8 +1,10 @@
 import server from '../src';
 import { FastifyInstance } from 'fastify';
-import {describe, expect, test} from '@jest/globals';
+import { describe, expect, test } from '@jest/globals';
+import { PrismaClient } from '@prisma/client';
 
 describe('api test', () => {
+
     test('ping test', async () => {
         const app: FastifyInstance = await server();
         await app.ready();
@@ -17,14 +19,68 @@ describe('api test', () => {
     });
 
     test('menu test', async () => {
+        const prisma = new PrismaClient();
+        const testStoreId = (await prisma.store.findFirst({
+            where: {
+                name: '소예다방'
+            }
+        }))?.id;
         const app: FastifyInstance = await server();
         await app.ready();
         const response = await app.inject({
             method: 'GET',
-            url: '/api/menu?storeId=1'
+            url: `/api/menu?storeId=${testStoreId}`
         });
-
         expect(response.statusCode).toBe(200);
+        const menu = JSON.parse(response.body).menus[0];
+        expect(menu.name).toBe("아메리카노");
+        expect(menu.price).toBe(2000);
+        expect(menu.storeId).toBe(testStoreId);
+        expect(menu.category).toBe("커피");
+
+        const menu2 = JSON.parse(response.body).menus[1];
+        expect(menu2.name).toBe("아이스티");
+        expect(menu2.price).toBe(2500);
+        expect(menu2.storeId).toBe(testStoreId);
+        expect(menu2.category).toBe("티&에이드");
+
+        const response2 = await app.inject({
+            method: 'POST',
+            url: `/api/payment/`,
+            payload: {
+                storeId: testStoreId,
+                menus: [
+                    {
+                        id: menu.id,
+                        count: 2
+                    },
+                    {
+                        id: menu2.id,
+                        count: 1
+                    }
+                ]
+            }
+        });
+        expect(response2.statusCode).toBe(200);
+        const payment = JSON.parse(response2.body);
+        expect(payment.paymentMethod).toBe("credit");
+        expect(payment.paymentStatus).toBe("paid");
+        expect(payment.totalPrice).toBe(6500);
+        expect(payment.orderitems.length).toBe(2);
+        expect(payment.orderitems[0].price).toBe(2000);
+        expect(payment.orderitems[0].count).toBe(2);
+        expect(payment.orderitems[1].price).toBe(2500);
+        expect(payment.orderitems[1].count).toBe(1);
+
+        const response3 = await app.inject({
+            method: 'GET',
+            url: `/api/payment/${testStoreId}`
+        });
+        expect(response3.statusCode).toBe(200);
+        const payments = JSON.parse(response3.body);
+        expect(payments.find((pm: any) => pm.id === payment.id)).not.toBeUndefined();
+        expect(payments.find((pm: any) => pm.id === payment.id).orderitems.length).toBe(2);
+
         await app.close();
     });
 });
