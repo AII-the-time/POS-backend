55d53c12e17a7d4638da71a70a85d9fe2ed4dfa5
raipen <raipendalk@gmail.com>
2023-07-23 06:09:16 +0900

    feat: update menu test

--- test/menuService.test.ts
+++ test/menuService.test.ts
@@ -36,6 +36,45 @@ describe('api test', () => {
             url: `/api/menu/${testStoreId}`
         });
         expect(response.statusCode).toBe(200);
-        await app.close();
+        const menu = JSON.parse(response.body).menus[0];
+        expect(menu.storeId).toBe(testStoreId);
+
+        const menu2 = JSON.parse(response.body).menus[1];
+
+        const response2 = await app.inject({
+            method: 'POST',
+            url: `/api/payment/`,
+            payload: {
+                storeId: testStoreId,
+                menus: [
+                    {
+                        id: menu.id,
+                        count: 2
+                    },
+                    {
+                        id: menu2.id,
+                        count: 1
+                    }
+                ]
+            }
+        });
+        expect(response2.statusCode).toBe(200);
+        const payment = JSON.parse(response2.body);
+        expect(payment.paymentMethod).toBe("credit");
+        expect(payment.paymentStatus).toBe("paid");
+        expect(payment.orderitems.length).toBe(2);
+        expect(payment.orderitems[0].count).toBe(2);
+        expect(payment.orderitems[1].count).toBe(1);
+
+        const response3 = await app.inject({
+            method: 'GET',
+            url: `/api/payment/${testStoreId}`
+        });
+        expect(response3.statusCode).toBe(200);
+        const payments = JSON.parse(response3.body);
+        expect(payments.find((pm: any) => pm.id === payment.id)).not.toBeUndefined();
+        expect(payments.find((pm: any) => pm.id === payment.id).orderitems.length).toBe(2);
+        
+        console.log("menu done");
     });
 });
