8350e35112f2709353aeb71705a1f0d91726104b
Jiwon <87058411+raipen@users.noreply.github.com>
2023-07-26 19:01:41 +0900

    refactor: separate service from api (#4)

--- src/api/payment.ts
+++ src/api/payment.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance,FastifyPluginAsync, FastifyRequest,FastifyReply } from "fastify";
-import { Menu } from '@prisma/client';
+import orderService from "../services/orderService";
 
 const api: FastifyPluginAsync =  async (server: FastifyInstance) => {
     server.post<{
@@ -13,45 +13,14 @@ const api: FastifyPluginAsync =  async (server: FastifyInstance) => {
     }>('/', async (request, reply) => {
         const storeId = request.body.storeId;
         const menus: {id:number,count:number}[] = request.body.menus;
-        const menusFromDatabase: Menu[] = await server.prisma.menu.findMany({
-            where: {
-                id: {
-                    in: menus.map((menu) => menu.id)
-                }
-            }
-        });
-        if(menus.filter((menu) => menusFromDatabase.find((menuFromDatabase) => menuFromDatabase.id === menu.id) === undefined).length > 0) {
+        try{
+            const payment = await orderService.orderMenus({server, storeId, menus});
+            reply.code(200).send(payment);
+        } catch(e) {
             return reply
                 .code(404)
                 .send();
         }
-
-        const realMenus: {menuId:number, count:number, price:number}[] = menus.map((menu) => {
-            return {
-                menuId: menu.id,
-                count: menu.count,
-                price: Number(menusFromDatabase.find((menuFromDatabase) => menuFromDatabase.id === menu.id)?.price)
-            }
-        });
-        const totalPrice = realMenus.reduce((acc, cur) => acc + cur.price * cur.count, 0);
-
-        const payment = await server.prisma.payment.create({
-            data: {
-                storeId: storeId,
-                paymentMethod: "credit",
-                paymentStatus: "paid",
-                totalPrice: totalPrice,
-                createdAt: new Date(),
-                updatedAt: new Date(),
-                orderitems: {
-                    create: realMenus
-                }
-            },
-            include: {
-                orderitems: true
-            }
-        });
-        reply.code(200).send(payment);
     });
 
     server.get<{
@@ -63,27 +32,15 @@ const api: FastifyPluginAsync =  async (server: FastifyInstance) => {
         }
     }>('/:storeId', async (request, reply) => {
         const storeId = Number(request.params.storeId);
-        if(await server.prisma.store.findFirst({
-            where: {
-                id: storeId
-            }
-        }) === null) {
+        try{
+            const payments = await orderService.getPayments(server,storeId);
+            reply.code(200).send(payments);
+        }
+        catch(e) {
             return reply
                 .code(404)
                 .send();
         }
-
-        const payments = await server.prisma.payment.findMany({
-            where: {
-                storeId: storeId
-            },
-            include: {
-                orderitems: true
-            }
-        });
-        reply
-            .code(200)
-            .send(payments);
     });
 }
 
--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,4 +1,63 @@
 import { FastifyInstance } from "fastify";
+import { Menu } from '@prisma/client';
 
 export default {
+    async orderMenus({server, storeId, menus}:{server:FastifyInstance, storeId:number,menus:{id:number,count:number}[]}): Promise<any>{
+        const menusFromDatabase: Menu[] = await server.prisma.menu.findMany({
+            where: {
+                id: {
+                    in: menus.map((menu) => menu.id)
+                }
+            }
+        });
+        if(menus.filter((menu) => menusFromDatabase.find((menuFromDatabase) => menuFromDatabase.id === menu.id) === undefined).length > 0) {
+            throw new Error("Menu not found");
+        }
+
+        const realMenus: {menuId:number, count:number, price:number}[] = menus.map((menu) => {
+            return {
+                menuId: menu.id,
+                count: menu.count,
+                price: Number(menusFromDatabase.find((menuFromDatabase) => menuFromDatabase.id === menu.id)?.price)
+            }
+        });
+        const totalPrice = realMenus.reduce((acc, cur) => acc + cur.price * cur.count, 0);
+
+        const payment = await server.prisma.payment.create({
+            data: {
+                storeId: storeId,
+                paymentMethod: "credit",
+                paymentStatus: "paid",
+                totalPrice: totalPrice,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                orderitems: {
+                    create: realMenus
+                }
+            },
+            include: {
+                orderitems: true
+            }
+        });
+        return payment;
+    },
+    async getPayments(server: FastifyInstance,storeId:number): Promise<any>{
+        if(await server.prisma.store.findFirst({
+            where: {
+                id: storeId
+            }
+        }) === null) {
+            throw new Error("Store not found");
+        }
+
+        const payments = await server.prisma.payment.findMany({
+            where: {
+                storeId: storeId
+            },
+            include: {
+                orderitems: true
+            }
+        });
+        return payments;
+    }
 }
