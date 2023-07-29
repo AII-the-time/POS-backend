80eb1061b7b0883e1841824f1a1f2a657101d4bb
raipen <raipendalk@gmail.com>
2023-07-29 18:30:17 +0900

    feat: update db models and seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,30 @@
 import { Prisma, PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 async function main() {
+    const user = await prisma.user.create({
+        data: {
+            businessRegistrationNumber: '0123456789',
+            phoneNumber: '010-1234-5678',
+        }
+    });
+
     const store = await prisma.store.create({
         data: {
+            userId: user.id,
             name: '소예다방',
             address: '고려대 근처',
-            phone: '010-1234-5678',
-            businessRegistrationNumber: '0123456789',
+            defaultOpeningHours: [
+                {
+                    "yoil": "일",
+                    "start": null,
+                    "end" : null //휴무일은 시작시간과 종료시간 모두 null 이어야함
+                },
+                {
+                    "yoil": "월",
+                    "start": "09:00",
+                    "end" : "18:00"
+                }
+            ],
             createdAt: new Date(),
             updatedAt: new Date(),
         },
@@ -18,6 +36,7 @@ async function main() {
                 price: 2000,
                 storeId: store.id,
                 category: '커피',
+                sort: 1,
                 createdAt: new Date(),
                 updatedAt: new Date(),
             },
@@ -26,6 +45,7 @@ async function main() {
                 price: 3000,
                 storeId: store.id,
                 category: '커피',
+                sort: 2,
                 createdAt: new Date(),
                 updatedAt: new Date(),
             },
@@ -34,6 +54,7 @@ async function main() {
                 price: 2500,
                 storeId: store.id,
                 category: '티&에이드',
+                sort: 3,
                 createdAt: new Date(),
                 updatedAt: new Date(),
             }
