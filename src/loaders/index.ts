ef2be223701b553897d0bfbd8ae71cff889cfb8c
raipen <raipendalk@gmail.com>
2023-09-02 20:14:32 +0900

    feat: change swagger info

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -10,8 +10,7 @@ export default async (server: FastifyInstance): Promise<void> => {
         prefix: '/docs',
         swagger: {
             info: {
-                title: 'Fastify API',
-                description: 'Building a blazing fast REST API with Node.js, MongoDB, Fastify and Swagger',
+                title: 'CAFEPOS API',
                 version: '0.1.0'
             },
             host: 'localhost:3000',
