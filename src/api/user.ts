371d26cde63c288a70027b3b9898bb470f5fece2
raipen <raipendalk@gmail.com>
2023-09-08 06:25:57 +0900

    feat: use schema sample

--- src/api/user.ts
+++ src/api/user.ts
@@ -3,23 +3,9 @@ import userService from "@services/userService";
 import * as User from "@DTO/user.dto";
 
 const api: FastifyPluginAsync = async (server: FastifyInstance) => {
-    server.post<{
-        Body: User.requestPhone,
-        Reply: {
-            200: User.responsePhone,
-            '4xx': undefined
-        }
-    }>('/phone', async (request, reply) => {
-        if (!request.body.phone) {
-            return reply
-                .code(400)
-                .send();
-        }
-        //TODO: 폰번호 형식 검사
-
-        const result: User.responsePhone = await userService.sendCertificationCode(request.body);
-        reply
-            .code(200)
+    server.post<User.phoneInterface>('/phone', {schema: User.phoneSchema}, async (request, reply) => {
+        const result = await userService.sendCertificationCode(request.body);
+        reply.code(200)
             .send(result);
     });
 
@@ -29,7 +15,7 @@ const api: FastifyPluginAsync = async (server: FastifyInstance) => {
             200: User.responseCertificatePhone,
             '4xx': undefined
         }
-    }>('/phone/certificationCode', async (request, reply) => {
+    }>('/phone/certificationCode', {schema: User.certificatePhoneSchema}, async (request, reply) => {
         if (!request.body.phone || !request.body.certificationCode || !request.body.phoneCertificationToken) {
             return reply
                 .code(400)
@@ -55,7 +41,7 @@ const api: FastifyPluginAsync = async (server: FastifyInstance) => {
             200: User.responseLogin,
             '4xx': undefined
         }
-    }>('/login', async (request, reply) => {
+    }>('/login', {schema: User.loginSchema}, async (request, reply) => {
         if (!request.body.businessRegistrationNumber || !request.body.certificatedPhoneToken) {
             return reply
                 .code(400)
@@ -74,13 +60,7 @@ const api: FastifyPluginAsync = async (server: FastifyInstance) => {
         }
     });
 
-    server.post<{
-        Headers: User.requestRefreshHeader,
-        Reply: {
-            200: User.responseRefresh,
-            '4xx': undefined
-        }
-    }>('/refresh', async (request, reply) => {
+    server.post<User.refreshInterface>('/refresh',{schema: User.refreshSchema}, async (request, reply) => {
         if (!request.headers.authorization) {
             return reply
                 .code(400)
@@ -88,7 +68,7 @@ const api: FastifyPluginAsync = async (server: FastifyInstance) => {
         }
 
         try {
-            const result: User.responseRefresh = await userService.refresh(request.headers);
+            const result = await userService.refresh(request.headers);
             reply
                 .code(200)
                 .send(result);
