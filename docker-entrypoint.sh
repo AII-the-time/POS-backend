99f653d7b39b9e15a78b818fbd5b002027711b63
Jiwon <87058411+raipen@users.noreply.github.com>
2023-09-13 05:34:00 +0900

    Cd: deploy to aws (#27)
    * feat: delete host of swagger at loader
    
    * cd: deploy to aws
    
    * fix: do not duplicate seed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,11 @@ echo "wait db server"
 ./wait-for-it.sh  $DB_HOST:$DB_PORT
 
 echo "start node server"
-npm run prisma
-npm run seed
+if [ "$NODE_ENV" = "production" ]; then
+    echo "production mode"
+    npm run prisma:generate
+else
+    npm run prisma
+    npm run seed
+fi
 npm run dev
