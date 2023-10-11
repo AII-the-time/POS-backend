dbd9bd61a69b885cbac2049eae2efffefe0a8d3d
raipen <raipendalk@gmail.com>
2023-10-11 15:01:43 +0900

    fix: delete wrong migration sqls

--- prisma/migrations/20231006085917_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `Recipe` ADD COLUMN `coldRegularAmount` INTEGER NULL,
-    ADD COLUMN `coldSizeUpAmount` INTEGER NULL,
-    ADD COLUMN `hotRegularAmount` INTEGER NULL,
-    ADD COLUMN `hotSizeUpAmount` INTEGER NULL;
