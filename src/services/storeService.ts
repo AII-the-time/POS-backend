ba9eb3f8e4b6c56ff2bf46eaee7edacaac04175a
Jiwon <87058411+raipen@users.noreply.github.com>
2023-10-23 22:47:18 +0900

    feat: seeding when new store create (#67)

--- src/services/storeService.ts
+++ src/services/storeService.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import * as Store from '@DTO/store.dto';
+import menuSeed from '../../prisma/menuSeed';
 const prisma = new PrismaClient();
 
 export default {
@@ -62,6 +63,10 @@ export default {
         storeId,
       })),
     });
+  },
+
+  async seeding({ storeId }: { storeId: number }): Promise<void> {
+    await menuSeed(prisma, storeId);
   },
 
   async getStoreList({
