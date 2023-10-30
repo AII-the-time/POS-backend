79a5c3b1e53790ce521b8b63bba1682fcdbcf94f
raipen <raipendalk@gmail.com>
2023-10-30 19:59:47 +0900

    refactor:rough of testing

--- test/integration/index.test.ts
+++ test/integration/index.test.ts
@@ -1,7 +1,6 @@
 import server from '@server';
 import { FastifyInstance } from 'fastify';
 import { afterAll, describe, expect, test } from '@jest/globals';
-import { LoginToken } from '@utils/jwt';
 import basic from './basic';
 import hook from './hook';
 
@@ -12,8 +11,21 @@ afterAll(async () => {
 });
 
 const tests:[string, (app: FastifyInstance) => () => void][] = [
-    ['basic test', basic],
-    ['hook test', hook],
+    ['basic', basic],
+    ['hook', hook],
+    ['login', (app: FastifyInstance) => () => {} ],
+    ['store CRU', (app: FastifyInstance) => () => {}],
+    ['get stock, mixed stock, menu, mileage before register', (app: FastifyInstance) => () => {}],
+    ['register stock, mixed stock, menu', (app: FastifyInstance) => () => {}],
+    ['get stock, mixed stock, menu after register', (app: FastifyInstance) => () => {}],
+    ['preorder', (app: FastifyInstance) => () => {}],
+    ['mileage', (app: FastifyInstance) => () => {}],
+    ['order', (app: FastifyInstance) => () => {}],
+    ['get stock, mixed stock, menu, mileage after order', (app: FastifyInstance) => () => {}],
+    ['update stock, mixed stock, menu', (app: FastifyInstance) => () => {}],
+    ['get order, stock, mixed stock, menu after update', (app: FastifyInstance) => () => {}],
+    ['delete order, stock, mixed stock, menu', (app: FastifyInstance) => () => {}],
+    ['get order, stock, mixed stock, menu, mileage after delete', (app: FastifyInstance) => () => {}],
 ];
 
 tests.forEach(([name, test]) => {
