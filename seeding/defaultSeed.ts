7f2242a7922d49e1e3464fcb53277b82a96b0d68
raipen <raipendalk@gmail.com>
2023-11-17 17:45:20 +0900

    feat: extract seeding function

---
+++ seeding/defaultSeed.ts
@@ -0,0 +1,83 @@
+import { PrismaClient } from '@prisma/client';
+
+export default async (prisma: PrismaClient, storeId: number) => {
+    await prisma.category.create({
+        data: {
+            name: '커피',
+            sort: 1,
+            storeId,
+        },
+    });
+    await prisma.stock.createMany({
+        data: [
+            {
+                name: '물',
+                unit: 'ml',
+                noticeThreshold: -1,
+                storeId,
+            },
+            {
+                name: '원두(예시)',
+                unit: 'g',
+                noticeThreshold: 1000,
+                currentAmount: 10000,
+                storeId,
+            },
+            {
+                name: '우유(예시)',
+                unit: 'ml',
+                noticeThreshold: 1000,
+                currentAmount: 500,
+                storeId,
+            }
+        ]
+    });
+
+    await prisma.menu.create({
+        data:
+        {
+            name: '아메리카노(예시)',
+            sort: 1,
+            price: 2500,
+            categoryId: 1,
+            storeId,
+            optionMenu: {
+                create: [1, 2, 3, 4, 5, 6].map((optionId) => ({ optionId }))
+            },
+            recipes: {
+                create: [
+                    {
+                        stockId: 1,
+                        coldRegularAmount: 150,
+                    }, {
+                        stockId: 2,
+                        coldRegularAmount: 25,
+                    }
+                ]
+            }
+        }
+    });
+    await prisma.menu.create({
+        data: {
+            name: '카페라떼(예시)',
+            price: 3000,
+            sort: 2,
+            categoryId: 1,
+            storeId,
+            optionMenu: {
+                create: [1, 2, 3, 4, 5, 6].map((optionId) => ({ optionId }))
+            },
+            recipes: {
+                create: [
+                    {
+                        stockId: 2,
+                        coldRegularAmount: 25,
+                    }, {
+                        stockId: 3,
+                        coldRegularAmount: 150,
+                    }
+                ]
+            }
+        }
+    });
+}
--- src/services/storeService.ts
+++ src/services/storeService.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 import * as Store from '@DTO/store.dto';
-import menuSeed from '@seeding/menuSeed';
+import testSeed from '@seeding/testSeed';
+import defaultSeed from '@seeding/defaultSeed';
 const prisma = new PrismaClient();
 
 export default {
@@ -69,90 +70,11 @@ export default {
   },
 
   async exampleSeeding({ storeId }: { storeId: number }): Promise<void> {
-    await prisma.category.create({
-      data: {
-        name: '커피',
-        sort: 1,
-        storeId,
-      },
-    });
-    await prisma.stock.createMany({
-      data: [
-        {
-          name: '물',
-          unit: 'ml',
-          noticeThreshold: -1,
-          storeId,
-        },
-        {
-          name: '원두(예시)',
-          unit: 'g',
-          noticeThreshold: 1000,
-          currentAmount: 10000,
-          storeId,
-        },
-        {
-          name: '우유(예시)',
-          unit: 'ml',
-          noticeThreshold: 1000,
-          currentAmount: 500,
-          storeId,
-        }
-      ]
-    });
-
-    await prisma.menu.create({
-      data:
-      {
-        name: '아메리카노(예시)',
-        sort: 1,
-        price: 2500,
-        categoryId: 1,
-        storeId,
-        optionMenu: {
-          create: [1,2,3,4,5,6].map((optionId) => ({optionId}))
-        },
-        recipes: {
-          create: [
-            {
-              stockId: 1,
-              coldRegularAmount: 150,
-            },{
-              stockId: 2,
-              coldRegularAmount: 25,
-            }
-          ]
-        }
-      }
-    });
-    await prisma.menu.create({
-        data: {
-          name: '카페라떼(예시)',
-          price: 3000,
-          sort: 2,
-          categoryId: 1,
-          storeId,
-          optionMenu: {
-            create: [1,2,3,4,5,6].map((optionId) => ({optionId}))
-          },
-          recipes: {
-            create: [
-              {
-                stockId: 2,
-                coldRegularAmount: 25,
-              },{
-                stockId: 3,
-                coldRegularAmount: 150,
-              }
-            ]
-          }
-        }
-    });
-
+    await defaultSeed(prisma, storeId);
   },
 
   async seeding({ storeId }: { storeId: number }): Promise<void> {
-    await menuSeed(prisma, storeId);
+    await testSeed(prisma, storeId);
   },
 
   async getStoreList({
