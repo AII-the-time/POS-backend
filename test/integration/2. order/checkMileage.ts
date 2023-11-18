37edf35372a4bf6214d14cee479fa25c0e82252d
raipen <raipendalk@gmail.com>
2023-11-18 18:32:10 +0900

    teat: change default test format

--- test/integration/2. order/checkMileage.ts
+++ test/integration/2. order/checkMileage.ts
@@ -1,8 +1,9 @@
 import { FastifyInstance } from 'fastify';
 import { ErrorInterface } from '@DTO/index.dto';
-import { LoginToken } from '@utils/jwt';
+import testValues from '../testValues';
+import * as Stock from '@DTO/stock.dto';
+import * as Menu from '@DTO/menu.dto';
 import { expect, test } from '@jest/globals';
 
 export default (app: FastifyInstance) => () => {
-    const accessToken = new LoginToken(1).signAccessToken();
 }
